fdaf9a066086861b5d57841412fba303f31b2e75
SuyeshBadge <suyeshbadge@gmail.com>
2021-03-20 18:07:14 +0530

    Babbar 450

--- BABBAR 450/4.0 1 2 arr sort.py
+++ BABBAR 450/4.0 1 2 arr sort.py
@@ -4,11 +4,6 @@ zc = arr.count(0)
 oc = arr.count(1)
 tc = arr.count(2)
 ans = []
-for i in range(len(arr)):
-    if i < zc:
-        ans.insert(0, 0)
-    elif i >= zc and i < oc+zc:
-        ans.append(1)
-    elif i >= oc and i < tc+oc+zc:
-        ans.append(2)
-print(ans)
+ans[0:zc] = [0]*zc
+ans[zc:oc] = [1]*oc
+ans[zc+oc:tc] = [2]*tc
---
+++ BABBAR 450/5.move -ve arr.py
@@ -0,0 +1,11 @@
+arr = [-12, 11, -13, -1, -5, 6, -7, 5, -3, -6]
+p = []
+n = []
+f = []
+for i in arr:
+    if i > 0:
+        p.append(i)
+    else:
+        n.append(i)
+
+f = n+p
