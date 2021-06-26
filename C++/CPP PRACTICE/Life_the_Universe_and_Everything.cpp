98eb48f4ddc7d9c6725e39bbde13b582fa05af74
Suyesh Badge <suyeshbadge@gmail.com>
2021-06-26 21:28:05 +0530

    C++

---
+++ C++/CPP PRACTICE/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while (true)
+    {
+        int n;
+        cin >> n;
+        if (n != 42)
+        {
+            cout << n << endl;
+            /* code */
+        }
+        else
+        {
+            break;
+        }
+    }
+    return 0;
+}
